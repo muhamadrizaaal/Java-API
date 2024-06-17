cf67baf305c4538b2cbc2ced47715321925ef27e
muhamadrizaaal <rizaaalmuhamad@gmail.com>
2024-06-17 17:05:22 +0700

    feat: create user and ERD

---
+++ java/src/main/java/com/java/java/controller/UserController.java
@@ -0,0 +1,58 @@
+package com.java.java.controller;
+
+import com.java.java.entity.User;
+import com.java.java.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        Optional<User> user = userService.findById(id);
+        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.save(user);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        Optional<User> user = userService.findById(id);
+        if (user.isPresent()) {
+            User existingUser = user.get();
+            existingUser.setUsername(userDetails.getUsername());
+            existingUser.setPassword(userDetails.getPassword());
+            existingUser.setEmail(userDetails.getEmail());
+            existingUser.setRole(userDetails.getRole());
+            return ResponseEntity.ok(userService.save(existingUser));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        if (userService.findById(id).isPresent()) {
+            userService.deleteById(id);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
---
+++ java/src/main/java/com/java/java/entity/User.java
@@ -0,0 +1,16 @@
+package com.java.java.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+
+    // getters and setters
+}
